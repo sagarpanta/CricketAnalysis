d0eb9ddcff7849dcae838aace4a2bc6bfae8c031
Sagar Pant <sagar_007p@yahoo.com>
2012-12-09 01:12:16 -0600

    added add_shottypes_to_shottype

---
+++ db/migrate/20121209071049_add_shottypes_to_shottype.rb
@@ -0,0 +1,38 @@
+class AddShottypesToShottype < ActiveRecord::Migration
+	SHOTS = [
+		{:shottype => 'Backfoot Cover Drive'},
+		{:shottype => 'Backfoot Defence'},
+		{:shottype => 'Backfoot Glance'},
+		{:shottype => 'Backfoot On Drive'},
+		{:shottype => 'Backfoot Straight Drive'},
+		{:shottype => 'Cover Drive'},
+		{:shottype => 'Dancing Down The Wicket'},
+		{:shottype => 'Defence'},
+		{:shottype => 'Edge'},
+		{:shottype => 'Extra Cover Drive'},
+		{:shottype => 'Flick'},
+		{:shottype => 'Glance'},
+		{:shottype => 'Guided Shot'},
+		{:shottype => 'Hook'},
+		{:shottype => 'Leave'},
+		{:shottype => 'On Drive'},
+		{:shottype => 'Off Drive'},
+		{:shottype => 'Paddle Sweep'},
+		{:shottype => 'Pull'},
+		{:shottype => 'Reverse Sweep'},
+		{:shottype => 'Scoop'},
+		{:shottype => 'Slug'},
+		{:shottype => 'Square Cut'},
+		{:shottype => 'Square Drive'},
+		{:shottype => 'Straight Drive'},
+		{:shottype => 'Sweep'},
+		{:shottype => 'Switch Hit'},
+		{:shottype => 'Tap'}
+	]
+	def change
+		SHOTS.each do |s|
+			st=Shottype.create(s)
+			st.save
+		end
+	end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121209065150) do
+ActiveRecord::Schema.define(:version => 20121209071049) do
 
   create_table "battings", :force => true do |t|
     t.integer  "teamkey"
