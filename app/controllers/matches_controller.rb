2aa4c1682fb1eba68a7acc16c74760dcb7795b13
Sagar Pant <sagar_007p@yahoo.com>
2012-12-11 12:54:47 -0600

    testing match_status controller

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -232,55 +232,56 @@ class MatchesController < ApplicationController
 			#@runsperover = Scorecard.find_by_sql('select ballnum, convert(varchar, inning) as inning,SUM(runs) as runs from scorecards s where clientkey= '+current_user.id.to_s+' and matchkey= '+params[:id].to_s+' group by inning, ballnum ')
 			
 			rpo_sql = '
-					select A.[over], convert(varchar,A.inning) as inning , ISNULL(SUM(runs+wides+noballs+legbyes+byes),0) as runs
+					select A."over", convert(varchar,A.inning) as inning , ISNULL(SUM(runs+wides+noballs+legbyes+byes),0) as runs
 					from
 					(
-					select distinct s.inning, s1.[over], s.clientkey, s.matchkey
+					select distinct s.inning, s1."over", s.clientkey, s.matchkey
 					from scorecards s cross join scorecards s1 
 					where  s.clientkey= '+current_user.id.to_s+' and s.matchkey= '+params[:id].to_s+'
 					and  s1.clientkey= '+current_user.id.to_s+' and s1.matchkey= '+params[:id].to_s+'
 					)A
-					LEFT join scorecards s on A.inning = s.inning and A.[over] = s.[over] and A.clientkey = s.clientkey and A.matchkey = s.matchkey
-
-					group by A.inning, A.[over] 
+					LEFT join scorecards s on A.inning = s.inning and A."over" = s."over" and A.clientkey = s.clientkey and A.matchkey = s.matchkey
+					group by A.inning, A."over" 
 					' 
 
 			crpo_sql = '
-					select A.[over], convert(varchar,A.inning) as inning , ISNULL(SUM(runs+wides+noballs+legbyes+byes),0) as runs
+					select A."over", to_char(inning, '+"'9'"+') as inning , ISNULL(SUM(runs+wides+noballs+legbyes+byes),0) as runs
 					from
 					(
-					select distinct s.inning, s1.[over], s.clientkey, s.matchkey
+					select distinct s.inning, s1."over", s.clientkey, s.matchkey
 					from scorecards s cross join scorecards s1 
 					where  s.clientkey= '+current_user.id.to_s+' and s.matchkey= '+params[:id].to_s+'
 					and  s1.clientkey= '+current_user.id.to_s+' and s1.matchkey= '+params[:id].to_s+'
 					)A
-					LEFT join scorecards s on A.inning = s.inning and A.[over] >= s.[over] and A.clientkey = s.clientkey and A.matchkey = s.matchkey
-
-					group by A.inning, A.[over] 
+					LEFT join scorecards s on A.inning = s.inning and A."over" >= s."over" and A.clientkey = s.clientkey and A.matchkey = s.matchkey
+					group by A.inning, A."over" 
 					'
 			@runsperover = Scorecard.find_by_sql(rpo_sql)
 			@cumulativerunsperover =  Scorecard.find_by_sql(crpo_sql)
 			
 			@currentinning = Scorecard.where('clientkey=? and matchkey=?', current_user.id, @matchid).select('max(inning) as inning')
-			@current = Scorecard.where('clientkey=? and matchkey=? and inning=?', current_user.id, @matchid, @currentinning[0].inning).select('SUM(runs+wides+noballs+legbyes+byes)/(max([over]*1.0)) as runrate, max([over]) as currentover, sum(runs) as score, max(ballnum) as currball')
-			@lastfiveRR = Scorecard.where('clientkey=? and matchkey=? and inning=?', current_user.id, @matchid, @currentinning[0].inning).select('SUM(runs+wides+noballs+legbyes+byes)/(count(distinct [over])*1.0) as runrate').where('[over] between '+(@current[0].currentover-4).to_s + ' and '+ @current[0].currentover.to_s)
+			@current = Scorecard.where('clientkey=? and matchkey=? and inning=?', current_user.id, @matchid, @currentinning[0].inning).select('SUM(runs+wides+noballs+legbyes+byes)/(max("over"*1.0)) as runrate, max("over") as currentover, sum(runs) as score, max(ballnum) as currball')
+
+			@five = @current[0].currentover.nil? ? 0:@current[0].currentover.to_i-4
+			@curr = @current[0].currentover.nil? ? 0:@current[0].currentover
+			@lastfiveRR = Scorecard.where('clientkey=? and matchkey=? and inning=?', current_user.id, @matchid, @currentinning[0].inning).select('SUM(runs+wides+noballs+legbyes+byes)/(count(distinct "over")*1.0) as runrate').where('"over" between '+(@five).to_s + ' and '+ (@curr).to_s)
 			@totalmatchballs = @match.matchovers * 6
 			
-			@currentoverindecimal = @current[0].currball/6 + @current[0].currball%6/6.0
-			@ballsremaining = @totalmatchballs - @current[0].currball
+			@currentoverindecimal = @current[0].currball.nil? ? 0:@current[0].currball.to_i/6 + @current[0].currball.to_i%6/6.0
+			@ballsremaining = @current[0].currball.nil? ? @totalmatchballs:@totalmatchballs - @current[0].currball.to_i
 			
 			@oversremaining = @ballsremaining/6+@ballsremaining%6/6.0
 			
-			@projectedwithCurrRR = (@current[0].runrate * @oversremaining).to_i + @current[0].score
+			@projectedwithCurrRR = @current[0].runrate.nil? ? 0:(@current[0].runrate.to_f * @oversremaining).to_i + @current[0].score.to_i
 			
-			@projectedwithsix = (6 * @oversremaining).to_i + @current[0].score
-			@projectedwitheight = (8 * @oversremaining).to_i + @current[0].score
+			@projectedwithsix = (6 * @oversremaining).to_i + (@current[0].score.nil? ? 0:@current[0].score.to_i)
+			@projectedwitheight = (8 * @oversremaining).to_i + (@current[0].score.nil? ? 0:@current[0].score.to_i)
 			
 			@arrRunsPerOver = Match.getChartData(@runsperover)
 			@dataRPO = Match.data_stringify(@arrRunsPerOver)
+			
 			@arrCumRunsPerOver = Match.getChartData(@cumulativerunsperover)
 			@dataCRPO = Match.data_stringify(@arrCumRunsPerOver)
-
 			respond_to do |format|
 				format.html	
 			end
