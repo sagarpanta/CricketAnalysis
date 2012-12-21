69ae7dbfc89817d54e812fa470cddfb55ec3cffb
Sagar Pant <sagar_007p@yahoo.com>
2012-12-21 01:30:13 -0600

    2nd conversion from tsql to pgsql standard

--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -366,7 +366,7 @@ class AnalysisController < ApplicationController
 			_group1['cr'] = 'cr'
 			_group1['pship'] = "case when currentstrikerkey<currentnonstrikerkey then p.fullname+'-'+p2.fullname else p2.fullname+'-'+p.fullname end"
 			#_group1['match'] = 's.matchkey'
-			_group1['match'] = "'vs '+tm1.teamname"
+			_group1['match'] = "'vs '||tm1.teamname"
 			_group1['shottype'] = 'st.shottype'
 			_group1['line'] = 'l.line'
 			_group1['length'] = 'ln.length'
@@ -399,7 +399,7 @@ class AnalysisController < ApplicationController
 			_group2['cr'] = ',cr'
 			_group2['pship'] = ",case when currentstrikerkey<currentnonstrikerkey then p.fullname+'-'+p2.fullname else p2.fullname+'-'+p.fullname end"
 			#_group2['match'] = ',s.matchkey'
-			_group2['match'] = ",'vs '+tm1.teamname"
+			_group2['match'] = ",'vs '||tm1.teamname"
 			_group2['shottype'] = ',st.shottype'
 			_group2['line'] = ',l.line'
 			_group2['length'] = ',ln.length'
@@ -787,7 +787,7 @@ class AnalysisController < ApplicationController
 			_group1['cr'] = 'cr'
 			_group1['pship'] = "case when currentstrikerkey<currentnonstrikerkey then p.fullname+'-'+p2.fullname else p2.fullname+'-'+p.fullname end"
 			#_group1['match'] = 's.matchkey'
-			_group1['match'] = "'vs '+tm.teamname"
+			_group1['match'] = "'vs '||tm.teamname"
 			_group1['shottype'] = 'st.shottype'
 			_group1['line'] = 'l.line'
 			_group1['length'] = 'ln.length'
@@ -820,7 +820,7 @@ class AnalysisController < ApplicationController
 			_group2['cr'] = ',cr'
 			_group2['pship'] = ",case when currentstrikerkey<currentnonstrikerkey then p.fullname+'-'+p2.fullname else p2.fullname+'-'+p.fullname end"
 			#_group2['match'] = ',s.matchkey'
-			_group2['match'] = ",'vs '+tm.teamname"
+			_group2['match'] = ",'vs '||tm.teamname"
 			_group2['shottype'] = ',st.shottype'
 			_group2['line'] = ',l.line'
 			_group2['length'] = ',ln.length'
