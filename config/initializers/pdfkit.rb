c280eebc8713512f26f888a280b323d28e0975bf
Sagar Pant <sagar_007p@yahoo.com>
2012-12-22 02:48:33 -0600

    fixing scorecard.html.erb

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,9 +1,10 @@
 # config/initializers/pdfkit.rb
 PDFKit.configure do |config|
    config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
+   #config.wkhtmltopdf = "wkhtmltopdf.exe"
    config.default_options = {
      :page_size => 'Legal',
      :print_media_type => true
    }
-   #config.root_url = "http://www.cricitdown.com" # Use only if your external hostname is unavailable on the server.
+   #config.root_url = "http://localhost" # Use only if your external hostname is unavailable on the server.
 end
