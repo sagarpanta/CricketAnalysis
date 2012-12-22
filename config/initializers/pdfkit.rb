385207bb6f6b720039d0ed4760b09d2d60402eaf
Sagar Pant <sagar_007p@yahoo.com>
2012-12-21 21:01:50 -0600

    root_url for pdfkit

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -5,5 +5,5 @@ PDFKit.configure do |config|
      :page_size => 'Legal',
      :print_media_type => true
    }
-   config.root_url = "http://shrouded-crag-2242.herokuapp.com" # Use only if your external hostname is unavailable on the server.
+   config.root_url = "http://www.cricitdown.com" # Use only if your external hostname is unavailable on the server.
 end
