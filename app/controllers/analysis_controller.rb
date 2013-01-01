506e89afe2e15fad1ba62a6964cf5e1964d238c6
Sagar Pant <sagar_007p@yahoo.com>
2013-01-01 14:43:19 -0600

    testing bbh15 with last X balls

--- app/controllers/analysis_controller.rb
+++ app/controllers/analysis_controller.rb
@@ -1264,7 +1264,7 @@ class AnalysisController < ApplicationController
 					select _rank, ballnum, grp1 '+(!_group2[group2].nil? ? ' ,grp2':'')+' , runs as val
 					from
 					(
-					select rank() over (order by  s.matchkey, inning,'+varB+_group1[group1]+(!_group2[group2].nil? ? _group2[group2]:'')+' , ballnum, noballs) as _rank, 
+					select rank() over (order by  s.matchkey, inning'+varB+_group1[group1]+(!_group2[group2].nil? ? _group2[group2]:'')+' , ballnum, noballs) as _rank, 
 						   ballnum,'+_group1[group1]+' as grp1 '+(!_group2[group2].nil? ? _group2[group2]+' as grp2':'')+', runs from '+scorecards+' s '+ _join + ' and wides=0
 					)A
 					where runs = 0
@@ -1290,7 +1290,7 @@ class AnalysisController < ApplicationController
 					select _rank, ballnum, grp1 '+(!_group2[group2].nil? ? ' ,grp2':'')+' , runs as val
 					from
 					(
-					select rank() over (order by  s.matchkey,inning,'+varA+_group1[group1]+(!_group2[group2].nil? ? _group2[group2]:'')+' , ballnum, noballs) as _rank, 
+					select rank() over (order by  s.matchkey,inning'+varB+_group1[group1]+(!_group2[group2].nil? ? _group2[group2]:'')+' , ballnum, noballs) as _rank, 
 						   ballnum,'+_group1[group1]+' as grp1 '+(!_group2[group2].nil? ? _group2[group2]+' as grp2':'')+', runs from '+scorecards+' s '+ _join + ' and wides=0
 					)A
 					where runs > 0
